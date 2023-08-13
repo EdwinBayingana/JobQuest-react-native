e42274e4f8390e7721c2b022d9c388fdbf26e405
EdwinBayingana <bayinganaedwin@gmail.com>
2023-08-13 13:35:22 +0200

    add a view containing the hello text in the Welcome component

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -10,11 +10,16 @@ import {
 import { useRouter } from 'expo-router';
 
 import styles from './welcome.style';
+import { icons, SIZES } from '../../../constants';
 
 const Welcome = () => {
+  const router = useRouter();
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Edwin</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect Job</Text>
+      </View>
     </View>
   );
 };
